67d71aba9d74f4d3b9a70f64849d51333c5ed724
jakzo <jack@jf.id.au>
2023-11-08 12:21:13 +1100

    delete old stacks

---
+++ scripts/delete-old-stacks.ts
@@ -0,0 +1,43 @@
+import path from "node:path";
+import * as pulumi from "@pulumi/pulumi";
+
+const ROOT_DIR = path.join(__dirname, "..");
+
+const PROTECTED_STACK_NAME = "rttw/prod";
+
+const run = async () => {
+  const workspace = await pulumi.automation.LocalWorkspace.create({
+    workDir: ROOT_DIR,
+  });
+
+  const stacks = await workspace.listStacks();
+
+  for (const stackSummary of stacks) {
+    if (stackSummary.name === PROTECTED_STACK_NAME) continue;
+
+    const stackName = stackSummary.name;
+    const stack = await pulumi.automation.LocalWorkspace.selectStack({
+      workDir: ROOT_DIR,
+      stackName,
+    });
+
+    const stackLastDeployDate = new Date(stackSummary.lastUpdate!);
+    console.log(
+      `Stack ${stackName} last deployed at ${stackSummary.lastUpdate}`
+    );
+    if (Date.now() - stackLastDeployDate.getTime() < 7 * 24 * 60 * 60 * 1000) {
+      console.log("Not deleting stack because it was deployed too recently");
+      continue;
+    }
+
+    console.log("Destroying stack");
+    await stack.destroy();
+    console.log("Stack has been destroyed");
+  }
+};
+
+if (require.main === module)
+  run().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
