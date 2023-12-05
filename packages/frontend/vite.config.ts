fae1bb43afbe26333418c33cee37eee324475264
acheronfail <acheronfail@gmail.com>
2023-12-05 13:12:58 +1030

    actions: add vite base config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }): UserConfig => {
   const isLocalDev = mode === "development";
   const API_BASE_URL = isLocalDev
     ? "http://api.execute-api.localhost.localstack.cloud:4566/stage"
     : "TODO";
   return {
+    base: "https://js-puzzles.github.io/JS-Puzzles/",
     define: {
       IS_LOCAL_DEV: isLocalDev,
       API_BASE_URL: JSON.stringify(API_BASE_URL),
