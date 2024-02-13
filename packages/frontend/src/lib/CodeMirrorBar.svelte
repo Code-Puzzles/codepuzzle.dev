fae1851fad7650ad5cc8fbb03e6bdcfed2823c76
acheronfail <acheronfail@gmail.com>
2024-02-13 20:50:34 +1030

    add disclaimer to frontend

--- packages/frontend/src/lib/CodeMirrorBar.svelte
+++ packages/frontend/src/lib/CodeMirrorBar.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-  import { Button } from "flowbite-svelte";
-  import { ChartBars3FromLeftSolid } from "flowbite-svelte-icons";
+  import { Button, Modal } from "flowbite-svelte";
+  import {
+    ChartBars3FromLeftSolid,
+    ExclamationCircleSolid,
+  } from "flowbite-svelte-icons";
+  import { fade } from "svelte/transition";
 
   export let showSolutionDisabled = false;
   export let showSolutionClicked: () => void;
   export let configureEditorClicked: () => void;
   export let showSidebarClicked: () => void;
+
+  let showDisclaimer = false;
 
   const divClass =
     "border-b-2 dark:border-gray-900 bg-gray-50 dark:bg-gray-800 flex flex-row justify-between py-2 px-3";
@@ -15,6 +21,16 @@
   <div>
     <Button size="sm" on:click={showSidebarClicked} class="md:hidden">
       <ChartBars3FromLeftSolid />
+    </Button>
+
+    <Button
+      size="sm"
+      on:click={() => (showDisclaimer = !showDisclaimer)}
+      class="text-yellow-200"
+      outline
+    >
+      <ExclamationCircleSolid />
+      &nbsp; Things not working?
     </Button>
   </div>
 
@@ -33,3 +49,19 @@
     </Button>
   </div>
 </div>
+
+{#if showDisclaimer}
+  <div transition:fade={{ duration: 300 }}>
+    <Modal
+      title="codepuzzle.dev isn't yet complete"
+      bind:open={showDisclaimer}
+      size="lg"
+      autoclose
+      outsideclose
+    >
+      Bear in mind this is not a finished product. Various things haven't been
+      built out fully yet! That said, feel free to play around and try things
+      out.
+    </Modal>
+  </div>
+{/if}
